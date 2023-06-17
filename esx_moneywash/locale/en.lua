774010bd78592e6b18b6def20e42bca96d1abe56
AnthoHansen <62178203+Antho7Twitch@users.noreply.github.com>
2023-06-17 18:39:53 +0200

    English localization added

---
+++ esx_moneywash/locale/en.lua
@@ -0,0 +1,6 @@
+Locales['en'] = {
+  ['start_washing'] = 'Washing your ~g~%s of dirty money!',
+  ['go_out'] = 'Run before the cops get here',
+  ['no_dmoney'] = 'You dont have any dirty money, come back when you do.',
+}
+
