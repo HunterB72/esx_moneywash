ac391e60ad48677ef05687262511d899db287318
AnthoHansen <62178203+Antho7Twitch@users.noreply.github.com>
2023-06-17 18:21:24 +0200

    Rename to fxmanifest and little tweaks

---
+++ esx_moneywash/fxmanifest.lua
@@ -0,0 +1,15 @@
+fx_version 'adamant'
+game 'gta5'
+
+server_scripts {
+    'server/main.lua'
+}
+
+shared_scripts {
+    'config.lua',
+    '@es_extended/imports.lua'
+}
+
+client_scripts {
+    'client/main.lua'
+}
