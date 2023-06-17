0698f8fa46f993c8770d1fb767b902b140f92b01
AnthoHansen <62178203+Antho7Twitch@users.noreply.github.com>
2023-06-17 19:40:08 +0200

    Refactor

--- esx_moneywash/server/main.lua
+++ esx_moneywash/server/main.lua
@@ -2,20 +2,23 @@ ESX = exports['es_extended']:getSharedObject()
 
 RegisterServerEvent('moneyWash:washMoney')
 AddEventHandler('moneyWash:washMoney', function()
-    local plr = ESX.GetPlayerFromId(source)
-    local blackMoney = plr.getAccount('black_money').money
-    if blackMoney ~= 0 then
-        plr.removeAccountMoney('black_money', blackMoney)
+    local src = source
+    local xPlayer = ESX.GetPlayerFromId(src)
+    local blackMoney = xPlayer.getAccount('black_money').money
+    if blackMoney >= Config.MinMoney then
+        
+        xPlayer.removeAccountMoney('black_money', blackMoney)
             
         local feeAmount = (blackMoney * Config.FeeAmount) / 100
             
-        TriggerClientEvent('esx:showNotification', plr.source, TranslateCap('start_washing'))
+        TriggerClientEvent('esx:showNotification', src, TranslateCap("start_washing", ESX.Math.GroupDigits(blackMoney)))
             
         Citizen.Wait(5000)
         
-        plr.addMoney(blackMoney - feeAmount)
-        TriggerClientEvent('esx:showNotification', plr.source, TranslateCap('go_out'))
+        xPlayer.addMoney(blackMoney - feeAmount)
+
+        TriggerClientEvent('esx:showNotification', src, TranslateCap("go_out", ESX.Math.GroupDigits(feeAmount)))
     else
-        TriggerClientEvent('esx:showNotification', plr.source, TranslateCap('no_dmoney'))
+        TriggerClientEvent('esx:showNotification', src, TranslateCap("no_dmoney", ESX.Math.GroupDigits(blackMoney)))
     end
 end)
